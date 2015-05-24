5889ce200dd5d75e072a5b3e8da77c5f2cefffbe
ryan.chiang <ryan.chiang@infortrend.com>
2015-05-24 23:01:43 +0800

    add return value

--- check-disk-usage.sh
+++ check-disk-usage.sh
@@ -4,26 +4,28 @@ declare -i disk_usage=`df | grep /dev/sda | awk {'print $5'} | cut -d'%' -f1`
 declare -i fc_slave_usage=`ssh ift@fc 'df | grep /dev/sda' | awk {'print $5'} | cut -d'%' -f1`
 declare -i iscsi_slave_usage=`ssh ift@iscsi 'df | grep /dev/sda' | awk {'print $5'} | cut -d'%' -f1`
 
+RET_BAD=0
+
 if [ "$disk_usage" -gt "40" ]; then
     echo "Master node Disk usage $disk_usage%, it's over 40%, please check!"
-    exit -1
+    RET_BAD=1
 else
     echo "Master node Disk usage $disk_usage%"
 fi
 
 if [ "$fc_slave_usage" -gt "40" ]; then
     echo "FC Slave    Disk usage $fc_slave_usage%, it's over 40%, please check!"
-    exit -2
+    RET_BAD=1
 else
     echo "FC Slave    Disk usage $fc_slave_usage%"
 fi
 
 if [ "$iscsi_slave_usage" -gt "40" ]; then
     echo "ISCSI Slave Disk usage $iscsi_slave_usage%, it's over 40%, please check!"
-    exit -3
+    RET_BAD=1
 else
     echo "ISCSI Slave Disk usage $iscsi_slave_usage%"
 fi
 
-exit 0
+exit $RET_BAD
 
--- check-slave-license.sh
+++ check-slave-license.sh
@@ -6,19 +6,21 @@ declare -i iscsi_license=`ssh ift@iscsi 'java -jar raidcmd_ESDS10.jar 10.10.10.2
 declare -i fc_license_day=`ssh ift@fc 'java -jar raidcmd_ESDS10.jar 10.10.10.200 show license' | grep EonPath | awk {'print $3'}`
 declare -i iscsi_license_day=`ssh ift@iscsi 'java -jar raidcmd_ESDS10.jar 10.10.10.200 show license' | grep EonPath | awk {'print $3'}`
 
+RET_BAD=0
+
 if [ "$fc_license" -gt "1" ]; then
     echo "FC Slave    license expires, please update license key!!"
-    exit -1
+    RET_BAD=1
 else
     echo "FC Slave    license remains $fc_license_day days"
 fi
 
 if [ "$iscsi_license" -gt "1" ]; then
     echo "ISCSI Slave license expires, please update license key!!"
-    exit -2
+    RET_BAD=1
 else
     echo "ISCSI Slave license remains $iscsi_license_day days"
 fi
 
-exit 0 
+exit $RET_BAD
 
