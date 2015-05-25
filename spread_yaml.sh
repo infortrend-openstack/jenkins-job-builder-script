32efc98dffb3917a30beee074161c7dd69421e4d
ryan.chiang <ryan.chiang@infortrend.com>
2015-05-25 10:17:36 +0800

    Change jenkins job name

--- spread_yaml.sh
+++ spread_yaml.sh
@@ -2,8 +2,8 @@
 
 FILE_ORIGI=/etc/jenkins_jobs/config/dsvm-cinder-driver.yaml
 FILE_SELF_FIBRE=/etc/jenkins_jobs/config/dsvm-cinder-driver-fc.yaml
-FILE_ISCSI=/etc/jenkins_jobs/config/ift-cinder-iscsi-volume.yaml
-FILE_FIBRE=/etc/jenkins_jobs/config/ift-cinder-fc-volume.yaml
+FILE_ISCSI=/etc/jenkins_jobs/config/infortrend-cinder-volume-iscsi.yaml
+FILE_FIBRE=/etc/jenkins_jobs/config/infortrend-cinder-volume-fc.yaml
 
 cp $FILE_ORIGI $FILE_SELF_FIBRE
 sed -i 's/dsvm-tempest-ift-driver-volume-test/dsvm-tempest-ift-driver-volume-test-fc/g' $FILE_SELF_FIBRE
@@ -13,12 +13,12 @@ sed -i 's/export IFT_RAID_CHL_MAP=4,5/export IFT_RAID_CHL_MAP=0,1/g' $FILE_SELF_
 sed -i 's/iSCSI/FC/g' $FILE_SELF_FIBRE
 
 cp $FILE_ORIGI $FILE_ISCSI
-sed -i 's/dsvm-tempest-ift-driver-volume-test/dsvm-tempest-ift-cinder-iscsi-volume/g' $FILE_ISCSI
+sed -i 's/dsvm-tempest-ift-driver-volume-test/infortrend-cinder-volume-iscsi/g' $FILE_ISCSI
 sed -i 's/devstack-logs/review-devstack-logs/g' $FILE_ISCSI
 sed -i 's/console-log/review-console-log/g' $FILE_ISCSI
 
 cp $FILE_ORIGI $FILE_FIBRE
-sed -i 's/dsvm-tempest-ift-driver-volume-test/dsvm-tempest-ift-cinder-fc-volume/g' $FILE_FIBRE
+sed -i 's/dsvm-tempest-ift-driver-volume-test/infortrend-cinder-volume-fc/g' $FILE_FIBRE
 sed -i 's/devstack-logs/review-devstack-logs/g' $FILE_FIBRE
 sed -i 's/console-log/review-console-log/g' $FILE_FIBRE
 sed -i 's/infortrend_iscsi_cli.InfortrendCLIISCSIDriver/infortrend_fc_cli.InfortrendCLIFCDriver/g' $FILE_FIBRE
